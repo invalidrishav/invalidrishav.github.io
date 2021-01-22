f617b7cf58932097e3e5e47502e750f004db2db0
invalidrishav <69727367+invalidrishav@users.noreply.github.com>
2021-01-22 23:07:13 +0530

    deleted

--- unsplash-test/assets/internet.js
+++
@@ -1,6 +0,0 @@
-// *CHECK IF THE USER IS CONNECTED TO THE INTERNET
-
-window.addEventListener('load', () => {
-    let text = document.getElementById('net_indicator');
-    if (!navigator.onLine) text.textContent = `You're Offline😢`;
-});
